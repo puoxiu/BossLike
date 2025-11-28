3acedb4a61a22dc42a7fcc71c039c9efa4300032
puoxiu <zmx_12345@163.com>
2025-11-28 21:29:41 +0800

    Initial Commit

---
+++ BOSS/BOSSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BOSSApp.swift
+//  BOSS
+//
+//  Created by xing on 2025/11/28.
+//
+
+import SwiftUI
+
+@main
+struct BOSSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BOSS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BOSS
+//
+//  Created by xing on 2025/11/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
